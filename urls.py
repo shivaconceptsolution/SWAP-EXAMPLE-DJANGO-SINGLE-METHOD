ec848b46e2fe4eafa43b84b92fa196c0cccc888c
shiva <50239824+shivaconceptsolution@users.noreply.github.com>
2021-03-25 13:07:56 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns =[
 
 path('',views.index,name='index'),
+path('operation',views.operation,name='operation')
 
 
 ]
--- views.py
+++ views.py
@@ -10,4 +10,19 @@ def index(request):
 		return render(request,"swap/index.html")
 
 
+def operation(request):
+	if request.method=="POST":
+		a = int(request.POST["txtnum1"])
+		b = int(request.POST["txtnum2"])
+		if request.POST['btn'] == "+":
+			c=a+b
+		elif request.POST['btn'] == "-":
+			c=a-b
+		elif request.POST['btn']=="*":
+			c=a*b
+		else:
+			c=a/b    
 
+		return render(request,"swap/ope.html",{'key1':a,'key2':b,'key':c})
+	else:
+		return render(request,"swap/ope.html")	
